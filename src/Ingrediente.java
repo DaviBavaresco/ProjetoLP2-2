395eb4dd5920cc984d55081eacc2b1c243f64651
DaviBavaresco <100875783+DaviBavaresco@users.noreply.github.com>
2023-02-01 18:26:38 -0300

    Initial commit

---
+++ src/Ingrediente.java
@@ -0,0 +1,25 @@
+public class Ingrediente {
+    private int quantidade;
+    private Condimento condimento;
+    private int total=0;
+
+    public Ingrediente(int quantidade, Condimento condimento) {
+        this.quantidade = quantidade;
+        this.condimento = condimento;
+    }
+
+    public int TotalProt(){
+      return total= quantidade*condimento.getProteina();
+    };
+    public int TotalCarb(){
+      return total=quantidade*condimento.getCarboidrato();
+    };
+
+    @Override
+    public String toString() {
+        return "Ingrediente{" +
+                "quantidade=" + quantidade +
+                ", condimento=" + condimento +
+                '}';
+    }
+}
